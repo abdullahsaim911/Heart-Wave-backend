a2d143dab08f2f09c1ec48c94c629c88665441f6
SaimOnKeys <145107382+abdullahsaim911@users.noreply.github.com>
2025-04-22 08:23:31 -0700

    Update taskRoutes.js

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -7,24 +7,30 @@ const router = express.Router();
 
 
 
-router.get('/mine', authMiddleware, roleMiddleware('ngo'), getMyTasks); //GET /api/tasks/my/:taskId
+router.get('/mine', authMiddleware, roleMiddleware('ngo'), getMyTasks); //GET /api/tasks/mine
 
-router.get('/', authMiddleware, getAllTasks);
+
+router.get('/', authMiddleware, getAllTasks); //GET /api/tasks
+
 
 router.get('/:taskId', authMiddleware, getTaskById); //GET /api/tasks/:taskId
-router.get('/my/:taskId', authMiddleware, roleMiddleware('ngo'), getMyTaskById);
-
-
-router.post('/', authMiddleware, roleMiddleware('ngo'), createTask);
-
-router.post('/assign', authMiddleware, roleMiddleware('ngo'), assignTask);
+router.get('/my/:taskId', authMiddleware, roleMiddleware('ngo'), getMyTaskById); //GET /api/tasks/my/:taskId
 
 
 
+router.post('/', authMiddleware, roleMiddleware('ngo'), createTask); //POST /api/tasks
 
-router.put('/:taskId', authMiddleware, roleMiddleware('ngo'), updateTask);
 
-router.delete('/:taskId', authMiddleware, roleMiddleware('ngo'), deleteTask);
+router.post('/assign', authMiddleware, roleMiddleware('ngo'), assignTask); // POST /api/tasks/assign
+
+
+
+
+
+router.put('/:taskId', authMiddleware, roleMiddleware('ngo'), updateTask); //PUT /api/tasks/:taskId
+
+
+router.delete('/:taskId', authMiddleware, roleMiddleware('ngo'), deleteTask); //DELETE /api/tasks/:taskId
 
 
 module.exports = router;
